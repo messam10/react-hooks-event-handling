a1a05915ac6258318f7e2944e764556de24bff20
messam10 <memaq1998.10.7@gmail.com>
2023-03-27 05:01:07 +0300

    Completed assignment

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ function Login() {
     <form onSubmit={handleSubmit}>
       <input type="text" name="username" placeholder="Enter username..." />
       <input type="password" name="password" placeholder="Enter password..." />
-      <button>Login</button>
+      <button onClick={handleSubmit}>Login</button>
     </form>
   );
 }
